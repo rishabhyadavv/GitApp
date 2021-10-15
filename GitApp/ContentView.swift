cbaf682265492555154e0887baa3ea0604933728
rishabhyadavv <rrishuyadav@gmail.com>
2021-10-15 10:42:47 +0200

    initial commit

---
+++ GitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitApp
+//
+//  Created by Rishabh Rishabh on 15.10.21.
+//  Copyright © 2021 Rishabh Rishabh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
